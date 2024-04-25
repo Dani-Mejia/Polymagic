18d1236203fdfc0f55f51d34d3e61bfb29823167
Dani-Mejia <danielmejiajovel@gmail.com>
2024-04-25 10:14:42 -0600

    forza

---
+++ main.js
@@ -0,0 +1,547 @@
+const productos = [
+
+    //Collares//
+    {
+        "id": "collar1",
+        "titulo": "Collar perlas Carita feliz",
+        "imagen": "Imagenes/Collares/Collar de perlas con caritas feliz.jpeg",
+        "categoria": {
+            "nombre": "Collares",
+            "id": "collares"
+        },
+        "precio": 4
+    },
+
+    {
+        "id": "collar2",
+        "titulo": "Collar mostacillas verdes",
+        "imagen": "Imagenes/Collares/collar mostacillas verdes.jpeg",
+        "categoria": {
+            "nombre": "Collares",
+            "id": "collares"
+        },
+        "precio": 4.50
+    },
+
+    {
+        "id": "collar3",
+        "titulo": "Collar perlas pequeñas",
+        "imagen": "Imagenes/Collares/Collar perlas pequeñas.jpeg",
+        "categoria": {
+            "nombre": "Collares",
+            "id": "collares"
+        },
+        "precio": 5
+    },
+
+    {
+        "id": "collar4",
+        "titulo": "Collar mostacillas azules y perlas",
+        "imagen": "Imagenes/Collares/collar mostacillas azules y perlas .jpeg",
+        "categoria": {
+            "nombre": "Collares",
+            "id": "collares"
+        },
+        "precio": 5
+    },
+
+    {
+        "id": "collar5",
+        "titulo": "Collar mostacilla blancas con inicial nombre",
+        "imagen": "Imagenes/Collares/Collar mostacilla blancas con inicial nombre.jpeg",
+        "categoria": {
+            "nombre": "Collares",
+            "id": "collares"
+        },
+        "precio": 3.50
+    },
+
+    {
+        "id": "collar6",
+        "titulo": "Collar mostacilla de colores",
+        "imagen": "Imagenes/Collares/collar mostacillas de colores.jpeg",
+        "categoria": {
+            "nombre": "Collares",
+            "id": "collares"
+        },
+        "precio": 5
+    },
+
+    {
+        "id": "collar7",
+        "titulo": "Collar mitad mostacillas y mitad perlas",
+        "imagen": "Imagenes/Collares/collar mitad mostacillas y mitad perlas.jpeg",
+        "categoria": {
+            "nombre": "Collares",
+            "id": "collares"
+        },
+        "precio": 3
+    },
+
+    {
+        "id": "collar8",
+        "titulo": "Collar mostacilla anaranjada y dije de corazon",
+        "imagen": "Imagenes/Collares/collar mostacilla anaranjada y dije de corazon .jpeg",
+        "categoria": {
+            "nombre": "Collares",
+            "id": "collares"
+        },
+        "precio": 3.50
+    },
+
+    {
+        "id": "collar9",
+        "titulo": "Collar mostacilla celeste, blanco y transparente",
+        "imagen": "Imagenes/Collares/Collar mostacilla celeste, blanco y transparente.jpeg",
+        "categoria": {
+            "nombre": "Collares",
+            "id": "collares"
+        },
+        "precio": 3
+    },
+
+    {
+        "id": "collar10",
+        "titulo": "Collar de perlas con caritas feliz",
+        "imagen": "Imagenes/Collares/Collar de perlas con caritas feliz.jpeg",
+        "categoria": {
+            "nombre": "Collares",
+            "id": "collares"
+        },
+        "precio": 5.50
+    },
+
+    {
+        "id": "collar11",
+        "titulo": "Collar mitad perla y mitad acero con inicial de nombre",
+        "imagen": "Imagenes/Collares/Collar mitad perla y mitad acero con inicial de nombre .jpeg",
+        "categoria": {
+            "nombre": "Collares",
+            "id": "collares"
+        },
+        "precio": 7
+    },
+
+    {
+        "id": "collar12",
+        "titulo": "Collar mitad de perlas y mitad de acero con dije de mariposa",
+        "imagen": "Imagenes/Collares/Collar mitad perlas y mitad acero con dije de mariposa.jpeg",
+        "categoria": {
+            "nombre": "Collares",
+            "id": "collares"
+        },
+        "precio": 7.50
+    },
+
+    {
+        "id": "collar13",
+        "titulo": "Collar mostacilla morada con inicial nombre",
+        "imagen": "Imagenes/Collares/Collar mostacilla morada con inicial nombre.jpeg",
+        "categoria": {
+            "nombre": "Collares",
+            "id": "collares"
+        },
+        "precio": 5
+    },
+
+    {
+        "id": "collar14",
+        "titulo": "Moztacilla piedra pequeña con dije mariposa",
+        "imagen": "Imagenes/Collares/Moztacilla piedra pequeña con dije mariposa .jpeg",
+        "categoria": {
+            "nombre": "Collares",
+            "id": "collares"
+        },
+        "precio": 4.75
+    },
+
+    {
+        "id": "collar15",
+        "titulo": "Collar de perlas ovaladas con corazones enlazados",
+        "imagen": "Imagenes/Collares/Collar de perlas ovaladas con corazones enlazados.jpeg",
+        "categoria": {
+            "nombre": "Collares",
+            "id": "collares"
+        },
+        "precio": 10
+    },
+
+    {
+        "id": "collar16",
+        "titulo": "Collar de perlas con dije de mariposa morada",
+        "imagen": "Imagenes/Collares/collar de perlas con dije de mariposa morada .jpeg",
+        "categoria": {
+            "nombre": "Collares",
+            "id": "collares"
+        },
+        "precio": 5.50
+    },
+
+    {
+        "id": "collar17",
+        "titulo": "Collar de acero con ojos de proteccion",
+        "imagen": "Imagenes/Collares/collar de acero con ojos de proteccion.jpeg",
+        "categoria": {
+            "nombre": "Collares",
+            "id": "collares"
+        },
+        "precio": 3.50
+    },
+
+    {
+        "id": "collar18",
+        "titulo": "Collar dos en uno de moztacillas pequeñas y nombre",
+        "imagen": "Imagenes/Collares/Collar dos en uno de moztacillas pequeñas y nombre .jpeg",
+        "categoria": {
+            "nombre": "Collares",
+            "id": "collares"
+        },
+        "precio": 8
+    },
+
+    {
+        "id": "collar19",
+        "titulo": "Collar dos en uno con letras de nombre",
+        "imagen": "Imagenes/Collares/Collar dos en uno con letras de nombre .jpeg",
+        "categoria": {
+            "nombre": "Collares",
+            "id": "collares"
+        },
+        "precio": 8
+    },
+
+    {
+        "id": "collar20",
+        "titulo": "Collar de mostacilla con inicial de nombres",
+        "imagen": "Imagenes/Collares/Collar de mostacilla con inicial de nombre s.jpeg",
+        "categoria": {
+            "nombre": "Collares",
+            "id": "collares"
+        },
+        "precio": 3.75
+    },
+
+    {
+        "id": "collar21",
+        "titulo": "Collar diferentes perlas con letras HUMBE",
+        "imagen": "Imagenes/Collares/Collar diferentes perlas con letras HUMBE.jpeg",
+        "categoria": {
+            "nombre": "Collares",
+            "id": "collares"
+        },
+        "precio": 5
+    },
+
+    {
+        "id": "collar22",
+        "titulo": "Collar de perlas blancas con dije de corazon pequeño",
+        "imagen": "Imagenes/Collares/Collar de perlas blancas con dije de corazon pequeño.jpeg",
+        "categoria": {
+            "nombre": "Collares",
+            "id": "collares"
+        },
+        "precio": 5
+    },
+
+    //Pendientes//
+    {
+        "id": "artes1",
+        "titulo": "Aretes caritas feliz",
+        "imagen": "Imagenes/Aretes/Aretes caritas feliz.jpeg",
+        "categoria": {
+            "nombre": "Aretes",
+            "id": "aretes"
+        },
+        "precio": 1.50
+    },
+
+    //Pulseras
+
+    {
+        "id": "pulseras1",
+        "titulo": "Pulsera de perlas y dije de flor (SQ)",
+        "imagen": "Imagenes/Pulseras/Pulsera de perlas y dije de flor (SQ).jpeg",
+        "categoria": {
+            "nombre": "Pulseras",
+            "id": "pulseras"
+        },
+        "precio": 2
+    },
+
+    {
+        "id": "pulseras2",
+        "titulo": "Pulseras duo de mostacillas",
+        "imagen": "Imagenes/Pulseras/Pulseras duo de mostacillas.jpeg",
+        "categoria": {
+            "nombre": "Pulseras",
+            "id": "pulseras"
+        },
+        "precio": 3
+    },
+
+    {
+        "id": "pulseras3",
+        "titulo": "Pulseras de hilo de bad bunny",
+        "imagen": "Imagenes/Pulseras/Pulsera de hilo de bad bunny.jpeg",
+        "categoria": {
+            "nombre": "Pulseras",
+            "id": "pulseras"
+        },
+        "precio": 2.50
+    },
+
+    {
+        "id": "pulseras4",
+        "titulo": "Pulseras de hilo de spiderman",
+        "imagen": "Imagenes/Pulseras/Pulseras de hilo de spiderman.jpeg",
+        "categoria": {
+            "nombre": "Pulseras",
+            "id": "pulseras"
+        },
+        "precio": 2.50
+    },
+
+    {
+        "id": "pulseras5",
+        "titulo": "Pulseras duo de hilo con dije corazon",
+        "imagen": "Imagenes/Pulseras/Pulseras duo de hilo con dije corazon.jpeg",
+        "categoria": {
+            "nombre": "Pulseras",
+            "id": "pulseras"
+        },
+        "precio": 5
+    },
+
+    {
+        "id": "pulseras6",
+        "titulo": "Pulseras de cuencas mostazilla de CINCO",
+        "imagen": "Imagenes/Pulseras/Pulseras de cuencas mostazilla de CINCO.jpeg",
+        "categoria": {
+            "nombre": "Pulseras",
+            "id": "pulseras"
+        },
+        "precio": 2.50
+    },
+
+    {
+        "id": "pulseras7",
+        "titulo": "Pulseras duo mostazillas negra y blanca con iniciales",
+        "imagen": "Imagenes/Pulseras/Pulseras duo mostazillas negra y blanca con iniciales .jpeg",
+        "categoria": {
+            "nombre": "Pulseras",
+            "id": "pulseras"
+        },
+        "precio": 5.50
+    },
+
+    {
+        "id": "pulseras8",
+        "titulo": "Pulseras duo rosa y negra piedras",
+        "imagen": "Imagenes/Pulseras/Pulseras duo rosa y negra piedras .jpeg",
+        "categoria": {
+            "nombre": "Pulseras",
+            "id": "pulseras"
+        },
+        "precio": 4
+    },
+
+    {
+        "id": "pulseras9",
+        "titulo": "Pulseras duo con inicial mostazilla tipo cristal",
+        "imagen": "Imagenes/Pulseras/Pulseras duo con inicial mostazilla tipo cristal .jpeg",
+        "categoria": {
+            "nombre": "Pulseras",
+            "id": "pulseras"
+        },
+        "precio": 8
+    },
+
+    {
+        "id": "pulseras10",
+        "titulo": "Pulsera de hilo y cristales de colores",
+        "imagen": "Imagenes/Pulseras/Pulsera de hilo y cristales de colores.jpeg",
+        "categoria": {
+            "nombre": "Pulseras",
+            "id": "pulseras"
+        },
+        "precio": 3.50  
+    },
+
+    //Llaveros
+
+    {
+        "id": "llaveros1",
+        "titulo": "LLaveros de mostacilla",
+        "imagen": "Imagenes/Llaveros/Llaveros de moztacilla.jpeg",
+        "categoria": {
+            "nombre": "Llaveros",
+            "id": "llaveros"
+        },
+        "precio": 1.75
+    },
+
+    //Anillos//
+
+    {
+        "id": "anillos1",
+        "titulo": "Anillos de mostacilla variados",
+        "imagen": "Imagenes/Anillos/anillos de mostacilla variados .jpeg",
+        "categoria": {
+            "nombre": "Anillos",
+            "id": "anillos"
+        },
+        "precio": 1
+    },
+
+    //PortaMask//
+
+    {
+        "id": "portamask1",
+        "titulo": "Porta mascarilla de flores",
+        "imagen": "Imagenes/PortaMask/Porta mascarilla de flores.jpeg",
+        "categoria": {
+            "nombre": "Porta mascarillas",
+            "id": "porta_mask"
+        },
+        "precio": 4
+    },
+
+
+];
+
+
+const contenedorProductos = document.querySelector("#contenedor_productos");
+const botonesCategorias = document.querySelectorAll(".boton_categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".Agregar_producto");
+const numerito = document.querySelector("#numerito");
+const searchInput = document.getElementById("#search-input");
+
+function cargarProductos(productosElegidos) {
+   
+    
+    contenedorProductos.innerHTML = "";
+    productosElegidos.forEach(producto => {
+
+
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+            <img class="imagen_producto" src="${producto.imagen}" alt="${producto.titulo}">
+                    <div class="producto_detalles">
+                        <h3 class="titulo_producto">${producto.titulo}</h3>
+                        <p class="precio producto">$${producto.precio}</p>
+                        <button class="Agregar_producto" id="${producto.id}">agregar</button>
+                    </div>
+                </div>
+        `;
+
+        contenedorProductos.append(div);
+    
+
+    })
+
+    actualizarBotonesAgregar()
+
+    
+}
+
+
+
+cargarProductos(productos);
+
+botonesCategorias.forEach(boton => {
+   boton.addEventListener("click", (e) => {
+    
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+        
+        if (e.currentTarget.id != "todos") {
+       
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre; 
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        
+        } else {
+            tituloPrincipal.innerText = "Todos los Productos";
+            cargarProductos(productos);
+        }
+
+    })
+});
+
+function actualizarBotonesAgregar() {
+    botonesAgregar = document.querySelectorAll(".Agregar_producto");
+
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+
+    });
+}
+
+let ProductosEnCarrito;
+let ProductosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+
+
+if(ProductosEnCarritoLS){
+    ProductosEnCarrito = JSON.parse(ProductosEnCarritoLS);
+    actualizarNumerito();   
+} else {
+    ProductosEnCarrito = [];
+}
+
+
+function agregarAlCarrito(e) {
+
+   
+    Toastify({
+        text: "Producto agregado",
+        duration: 3000,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #db8fee, #f79aa4)",
+          borderRadius: "2rem",
+          textTransform: "uppercase",
+          fontSize: ".75rem"
+        },
+        offset: {
+            x: '1.5rem', // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+            y: '1.5rem' // vertical axis - can be a number or a string indicating unity. eg: '2em'
+          },
+        onClick: function(){} // Callback after click
+      }).showToast();
+
+
+    const idBoton = e.currentTarget.id;
+    const ProductoAgregado = productos.find(producto => producto.id === idBoton);
+
+    if(ProductosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = ProductosEnCarrito.findIndex(producto => producto.id === idBoton);
+        ProductosEnCarrito[index].cantidad++;
+    } else {
+        ProductoAgregado.cantidad = 1;
+        ProductosEnCarrito.push(ProductoAgregado); 
+    }
+    
+    actualizarNumerito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(ProductosEnCarrito));
+}
+
+function actualizarNumerito(){
+    let nuevoNumerito = ProductosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerHTML = nuevoNumerito
+    
+
+}
+
+
+
+
+
+
